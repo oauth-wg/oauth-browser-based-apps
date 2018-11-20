758cddbd1a4babcb23f72b2d6bb5380633fd17a3
Aaron Parecki <aaron@parecki.com>
2018-11-19 16:40:37 -0800

    update convert script to run through ietf converter

--- convert.sh
+++ convert.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 kramdown-rfc2629 oauth-browser-based-apps.md > build/oauth-browser-based-apps.xml
+
+curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-browser-based-apps.xml -F 'modeAsFormat=txt/ascii' -F type=ascii > build/oauth-browser-based-apps.txt
+curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-browser-based-apps.xml -F 'modeAsFormat=html/ascii' -F type=ascii > build/oauth-browser-based-apps.html
+
+sed -i .bak 's/, \.//' build/oauth-browser-based-apps.txt
+rm build/oauth-browser-based-apps.txt.bak
+
+sed -i .bak 's/, \.//' build/oauth-browser-based-apps.html
+rm build/oauth-browser-based-apps.html.bak
