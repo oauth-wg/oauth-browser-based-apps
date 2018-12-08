96ad92cabb01e53139ff636ce6da0e17b942b14b
Aaron Parecki <aaron@parecki.com>
2018-12-08 13:39:18 -0800

    fix working group and ref author info

--- convert.sh
+++ convert.sh
@@ -2,11 +2,10 @@
 
 kramdown-rfc2629 oauth-browser-based-apps.md > build/oauth-browser-based-apps.xml
 
+./replacements.php
+
 curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-browser-based-apps.xml -F 'modeAsFormat=txt/ascii' -F type=ascii > build/oauth-browser-based-apps.txt
 curl https://xml2rfc.tools.ietf.org/cgi-bin/xml2rfc.cgi -F input=@build/oauth-browser-based-apps.xml -F 'modeAsFormat=html/ascii' -F type=ascii > build/oauth-browser-based-apps.html
 
-sed -i .bak 's/, \.//' build/oauth-browser-based-apps.txt
-rm build/oauth-browser-based-apps.txt.bak
+# https://datatracker.ietf.org/submit/
 
-sed -i .bak 's/, \.//' build/oauth-browser-based-apps.html
-rm build/oauth-browser-based-apps.html.bak
